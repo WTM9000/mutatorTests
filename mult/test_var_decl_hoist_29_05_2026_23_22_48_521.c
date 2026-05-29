d95b20b883ad920cf5b17a29953c617a8826b8d8
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-29 23:22:49 +0300

    Date: 29.05.2026, Time: 23:22:48 Files generated:  test_var_decl_hoist_29_05_2026_23_22_48_521.c

--- mult/test_var_decl_hoist_29_05_2026_23_22_48_521.c
+++ mult/test_var_decl_hoist_29_05_2026_23_22_48_521.c
@@ -1,6 +1,6 @@
 int main() {
+    int data[] = {2, 3, 0, 4, 3, 7};
     printf("fib_mod(10,1000): %d\n", fibonacci_mod(10, 1000));
-    int data[6] = {2, 3, 0, 4, 3, 7};
     printf("product skip zero: %d\n", array_product(data, 6));
     printf("unique count: %d\n", count_unique(data, 6));
     classify_numbers(data, 6);
