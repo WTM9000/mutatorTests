91c0b0f10ec0f667fa8edf01819e358733f34448
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-29 22:56:25 +0300

    Date: 29.05.2026, Time: 22:56:25 Files generated:  25_mixed_all__continue_unreachable__return_unreachable__varname_replace_29_05_2026_22_56_25_609.c

---
+++ mult/test_var.c
@@ -0,0 +1,14 @@
+int array_product(int *arr, int n) {
+    int prod = 1;
+    int i = 0;
+    while (i < n) {
+        if (arr[i] == 0) {
+            i = i + 1;
+            continue;
+        }
+        prod = prod * arr[i];
+        i = i + 1;
+        continue;
+    }
+    return prod;
+}
