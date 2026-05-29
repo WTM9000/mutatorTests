7b6052dcb53a846e0fd40776d933f863b1ceca2a
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-29 13:55:11 +0300

    Date: 29.05.2026, Time: 13:55:11 Files generated:  test01_braced_if_variant_a__return_unreachable_29_05_2026_13_55_11_301.c

---
+++ returnUnreach/test01_braced_if_variant_a__return_unreachable_29_05_2026_13_55_11_301.c
@@ -0,0 +1,16 @@
+/* test01 - return inside braced if-body, Variant A available.
+ * needsBraces = false (parent Block exists between IfStatement and ReturnStatement).
+ * Expected: dead expr inserted as new line after "return x;", same indent.
+ *
+ *   if (a > 0) {
+ *       return x;
+ *       x++;      <- inserted
+ *   }                                                          */
+int compute(int a, int b) {
+    int x = a + b;
+    if (a > 0) {
+        return x;
+    }
+    return 0;
+    ++x;
+}
