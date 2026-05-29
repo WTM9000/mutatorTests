d15a2ec0e2691875c6e5ac97f14ce5a454cdbdda
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-29 13:47:05 +0300

    Date: 29.05.2026, Time: 13:47:05 Files generated:  test_elseif_01__elseif_flatten_29_05_2026_13_47_05_218.c

---
+++ flatIf/test_elseif_01__elseif_flatten_29_05_2026_13_47_05_218.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x < 0) {
+        printf("negative\n");
+    } else {
+        if (x == 0) {
+            printf("zero\n");
+        } else {
+            printf("positive\n");
+        }
+    }
+    return 0;
+}
